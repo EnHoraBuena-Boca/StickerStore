c95b4dde3bb8431a1e20b54cee3dff9c8f45c1a2
EnHoraBuena-Boca <matiasxav@proton.me>
2025-12-15 23:08:02 -0500

    further fix ..env

--- back/config/application.rb
+++ back/config/application.rb
@@ -1,6 +1,9 @@
 require_relative "boot"
 
 require "rails/all"
+require "dotenv/load"
+
+Dotenv.load(File.expand_path("../../.env", __dir__))
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
