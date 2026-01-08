2b9a8fb82b5be52bedfd904d28d1fa18f4799ccb
EnHoraBuena-Boca <matiasxav@proton.me>
2026-01-07 23:31:56 -0500

    finish refactoring of card upload page

--- back/app/controllers/api/v1/original_cards_controller.rb
+++ back/app/controllers/api/v1/original_cards_controller.rb
@@ -19,37 +19,43 @@ class Api::V1::OriginalCardsController < ApplicationController
   end
 
   def approved
+    api_results = []
     for i in params[:ids] 
       unapproved_card = OriginalCard.find_by(id: i)
-      result = Cloudinary::Api.update("Stickers/Unapproved/"+unapproved_card.api_id,asset_folder: "Stickers/"+unapproved_card.cardtype)
+      result = Cloudinary::Api.update("#{unapproved_card.season}/Unapproved/#{unapproved_card.api_id}",asset_folder: "#{unapproved_card.season}/#{unapproved_card.cardtype}/")
       if result
         unapproved_card.update_attribute(:approved, true)
-        render json: unapproved_card, status: :ok
+        api_results.append(result)
       else 
         render json: result, status: :unprocessable_content
       end
     end
+    render json: api_results, status: :ok
+  end
+
+  def delete_cards
+    for i in params[:ids] 
+      unapproved_card = OriginalCard.find_by(id: i)
+      result = Cloudinary::Uploader.destroy("#{unapproved_card.season}/Unapproved/#{unapproved_card.api_id}")
+      if result
+        unapproved_card.destroy
+      else 
+        render json: result, status: :unprocessable_content
+      end
+    end
+    render status: :ok
 
   end
 
   # POST /original_cards
   def create
-    user = User.find_by(id: session[:current_user_id])
+    # user = User.find_by(id: session[:current_user_id])
+    r = scan_zip(params[:zip])
 
-    if user.card_approver?
-      result = Cloudinary::Uploader.upload(params[:image], folder: "Stickers/"+params[:cardtype])
-      result['public_id'].slice! "Stickers/"+params[:cardtype]+"/"
-      @original_card = OriginalCard.new(name: params[:name], cardtype: params[:cardtype], approved: true, api_id: result['public_id'])
-    else 
-      result = Cloudinary::Uploader.upload(params[:image], folder: "Stickers/Unapproved")
-      result['public_id'].slice! "Stickers/Unapproved/"
-      @original_card = OriginalCard.new(name: params[:name], cardtype: params[:cardtype], approved: false, api_id: result['public_id'])
-    end
-
-    if @original_card.save
-      render json: @original_card, status: :created, location: @original_card
+    if r == false
+      render status: :unprocessable_content
     else
-      render json: @original_card.errors, status: :unprocessable_content
+      render json: r, status: :created, location: @original_card
     end
   end
 
@@ -64,7 +70,7 @@ class Api::V1::OriginalCardsController < ApplicationController
 
   # DELETE /original_cards/1
   def destroy
-    @original_card.destroy!
+    puts request.body.read
   end
 
   private
@@ -76,5 +82,57 @@ class Api::V1::OriginalCardsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def original_card_params
       params.fetch(:original_card, {})
+    end
+
+    def scan_zip(zip)
+      cards = []
+      Zip::InputStream.open(zip) do |zip_stream|
+        while entry = zip_stream.get_next_entry
+          next if entry.directory?
+          if !File.basename(entry.name).include? ".png"
+            return false
+          end
+          tempfile = Tempfile.new(File.basename(entry.name))
+          tempfile.binmode
+          tempfile.write(zip_stream.read)
+          tempfile.rewind
+
+          file_name_args = get_file_name_args(File.basename(entry.name))
+          result = create_original_card(tempfile,file_name_args)
+
+          if result
+            cards.append(result) 
+          else 
+            return result
+          end
+        end
+      end
+      return cards 
+    end
+
+    def get_file_name_args(file_name)
+      elements = file_name.split('-')
+      elements[0] = elements[0].tr!("_", " ")
+      elements[2] = elements[2][/\d\d/]
+      return elements
+    end
+
+    def create_original_card(file, file_args)
+      user = User.find_by(id: session[:current_user_id])
+      if user.card_approver?
+        result = Cloudinary::Uploader.upload(file, folder: file_args[2]+"/"+file_args[1].capitalize)
+        result['public_id'].slice! file_args[2]+"/"+file_args[1]+"/"
+        original_card = OriginalCard.new(name: file_args[0], cardtype: file_args[1].capitalize, approved: true, api_id: result['public_id'], season:file_args[2])
+      else 
+        result = Cloudinary::Uploader.upload(file, folder: file_args[2]+"/Unapproved")
+        result['public_id'].slice! file_args[2]+"/Unapproved/"
+        original_card = OriginalCard.new(name: file_args[0], cardtype: file_args[1].capitalize, approved: false, api_id: result['public_id'], season:file_args[2])
+      end
+      
+      if original_card.save
+        return original_card
+      else
+        return false
+      end
     end
 end
